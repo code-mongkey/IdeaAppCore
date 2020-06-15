294340197307be153210951f9ef741739e8070c4
hgh <ymca0147@naver.com>
2020-06-15 12:45:07 +0900

    PortfoilioWebsite 컴포넌트 추가 및 버틀 클릭에 대한 이번트 처리기 구현 그리고 NavigationManager 참조 사용

---
+++ Pages/Portfolios/PortfolioWebsite.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace IdeaAppCore.Pages.Portfolios
+{
+    public partial class PortfolioWebsite
+    {
+        [Inject]
+        public NavigationManager NavigationManagerReference { get; set; }
+
+        protected override void OnInitialized()
+        {
+        }
+
+        private void GoToPortSites()
+        {
+            NavigationManagerReference.NavigateTo("/Index");
+        }
+    }
+}
