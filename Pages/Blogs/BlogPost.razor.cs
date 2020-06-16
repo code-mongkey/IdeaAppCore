cc4f750d50290fb0a5551c48c552373748ffd725
haeng <ymca0147@naver.com>
2020-06-17 00:08:48 +0900

    Modern Business Templete Clone Complete

---
+++ Pages/Blogs/BlogPost.razor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdeaAppCore.Pages.Blogs
+{
+    public partial class BlogPost
+    {
+    }
+}
