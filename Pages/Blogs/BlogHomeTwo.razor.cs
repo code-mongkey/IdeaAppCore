e46a591d97fa0954e0b821faa85276b0262e2ee8
hgh <ymca0147@naver.com>
2020-06-16 18:07:58 +0900

    Add Other Pages

---
+++ Pages/Blogs/BlogHomeTwo.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Components;
+
+namespace IdeaAppCore.Pages.Portfolios
+{
+    public partial class BlogHomeTwo
+    {
+    }
+}
---
+++ Pages/Contact.razor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdeaAppCore.Pages
+{
+    public partial class Contact
+    {
+        private string title = "타이틀";
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+    }
+}
